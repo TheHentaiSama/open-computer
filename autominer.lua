cce7881cac8f2a3b63e57aaab676cf2148ab3c82
Valentin Gautier <Valentin.Gautier@grenoble-inp.fr>
2019-05-19 09:11:06 +0200

    fin

---
+++ autominer.lua
@@ -0,0 +1,31 @@
+local robot = require("robot")
+
+local function miner(n)
+   for i=0,n-1 do
+      robot.swing()
+      robot.forward()
+      robot.swingUp()
+   end
+end
+
+local function retour(n)
+   robot.turnLeft()
+   robot.turnLeft()
+   for i=0,n-1 do
+      robot.forward()
+   end
+end
+
+local profondeur = arg[1]
+local nombre = arg[2]
+
+for i=0,nombre-1 do
+   miner(profondeur)
+   retour(profondeur)
+   robot.turnRight()
+   robot.turnRight()
+   robot.forward()
+   robot.forward()
+   robot.turnRight()
+   robot.turnRight()
+end
